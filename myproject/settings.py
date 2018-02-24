5895c43fdaddd38422edc17f66a1c580bb94a50a
petr3460 <petr3460@gmail.com>
2018-02-24 15:56:36 +0500

    add Postresql

--- myproject/settings.py
+++ myproject/settings.py
@@ -74,11 +74,24 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'petr3460',
+        'PASSWORD': 'Asdfg133',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
